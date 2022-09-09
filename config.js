0da9f11ee07932db5b58a8a94b81d689a4af3e9c
serbinov.oleksandr <serbinov.oleksandr@teamharbour.com>
2022-09-09 05:48:10 +0300

    new contract

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const verifyingContract = '0xe6214fdfE72537874f4a66a293B44BB854F808da';
+const verifyingContract = '0xCc02C16daBd8AADeef2DCA2A06ae01F83e0e1ee9';
 const chainId = 5;
 const verifierKey = 'fdeed0f82e24e299f6b7c5c9d286fce042cb67911941f4c0be75841d1e898c6e';
 const verifierAddress = '0xa1e1fB25268cEfB55225dbE5fD63a3b44D35E6aA';
