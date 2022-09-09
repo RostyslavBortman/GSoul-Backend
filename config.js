b896cfde524e4fb964dc2c62a5e5c077387afdad
serbinov.oleksandr <serbinov.oleksandr@teamharbour.com>
2022-09-09 05:23:32 +0300

    soul updated

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const verifyingContract = '0x55A84B89CB4da7Ab78BCe0EB256D51E64d5d7f25';
+const verifyingContract = '0xe6214fdfE72537874f4a66a293B44BB854F808da';
 const chainId = 5;
 const verifierKey = 'fdeed0f82e24e299f6b7c5c9d286fce042cb67911941f4c0be75841d1e898c6e';
 const verifierAddress = '0xa1e1fB25268cEfB55225dbE5fD63a3b44D35E6aA';
