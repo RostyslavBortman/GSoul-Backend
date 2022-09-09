a7e74d1cbd58a878fc5c051c107b61a4bed292e8
serbinov.oleksandr <serbinov.oleksandr@teamharbour.com>
2022-09-09 05:00:31 +0300

    chahgen soulbound contract

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const verifyingContract = '0x6AF941bCa64baC55a76dD02341a29d5D85958A22';
+const verifyingContract = '0x55A84B89CB4da7Ab78BCe0EB256D51E64d5d7f25';
 const chainId = 5;
 const verifierKey = 'fdeed0f82e24e299f6b7c5c9d286fce042cb67911941f4c0be75841d1e898c6e';
 const verifierAddress = '0xa1e1fB25268cEfB55225dbE5fD63a3b44D35E6aA';
